3d1dea6e13be17ca55cf585e8da2f0461b1d6314
Ana Clara Pereira <88745740+ana-epereira@users.noreply.github.com>
2024-02-18 16:59:39 -0300

    att

--- api/index.py
+++ api/index.py
@@ -1,13 +1,18 @@
 from flask import Flask
-#import pandas as pd
-
-#df = pd.read_csv('Ghibli characters.csv')
+from pandas import pandas
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Teste'
+
+@app.route('/')
+def antigo():
+    df = pandas.read_csv('Ghibli characters.csv')
+    antigo = df['movie'][df['release date'] == df['release date'].min()].drop_duplicates().to_string()
+    antigo = antigo[2:]
+    return 'O filme mais antigo é {antigo}'
 
 @app.route('/about')
 def about():

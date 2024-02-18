5facfc7a848a1247bca9cd3b3969f0e3494229e7
Ana Clara Pereira <88745740+ana-epereira@users.noreply.github.com>
2024-02-18 17:39:34 -0300

    att

--- api/index.py
+++ api/index.py
@@ -1,31 +1,11 @@
-from flask import Flask,request, redirect, url_for
-from io import TextIOWrapper
-from flask_sqlalchemy import SQLAlchemy
-#from pandas import pandas
+from flask import Flask
+from pandas import pandas
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Teste'
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_csv():
-    if request.method == 'POST':
-        csv_file = request.files['file']
-        csv_file = TextIOWrapper(csv_file, encoding='utf-8')
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            user = User(username=row[0], email=row[1])
-            db.session.add(user)
-            db.session.commit()
-        return redirect(url_for('upload_csv'))
-    return """
-            <form method='post' action='/' enctype='multipart/form-data'>
-              Upload a csv file: <input type='file' name='file'>
-              <input type='submit' value='Upload'>
-            </form>
-           """
 
 @app.route('/ghibli')
 def antigo():

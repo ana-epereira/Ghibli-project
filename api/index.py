6e81f2b8a63a62dc8a72c861c403dd6b4aaf4622
Ana Clara Pereira <88745740+ana-epereira@users.noreply.github.com>
2024-02-18 17:25:33 -0300

    att

---
+++ api/index.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from pandas import pandas
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Teste'
+
+@app.route('/ghibli')
+def antigo():
+    df = pandas.read_csv('Ghibli characters.csv')
+    antigo = df['movie'][df['release date'] == df['release date'].min()].drop_duplicates().to_string()
+    antigo = antigo[2:]
+    return 'O filme mais antigo é {antigo}'
+
+@app.route('/about')
+def about():
+    return 'About'
+
